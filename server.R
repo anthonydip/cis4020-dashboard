3cb57405254b86052ebbbd13a519151098697796
Michael Lam <m.lam08@outlook.com>
2023-11-30 22:55:09 -0500

    urban vs rural data & confusion matrix panel

--- server.R
+++ server.R
@@ -6,6 +6,9 @@ library(tidyr)
 library(e1071)
 library(caret)
 library(Rtsne) # For t-SNE visualization
+library(arrow)
+library(readr)
+library(caret)
 
 # Run Canine vs Feline SVM before application loads
 run_cvf_svm <- function() {
@@ -72,10 +75,133 @@ run_cvf_svm <- function() {
   return(results)
 }
 
+add_regions <- function(dataframe) {
+  dataframe$region = NA
+  for (i in 1:nrow(dataframe)) {
+    dataframe$region[i] = switch(
+      dataframe$county[i],
+      "New York" = "Urban",
+      "Nassau" = "Urban",
+      "Suffolk" = "Urban",
+      "Kings" = "Urban",
+      "Westchester" = "Urban",
+      "Albany" = "Rural",
+      "Monroe" = "Rural",
+      "Onondaga" = "Rural",
+      "Erie" = "Rural",
+      "Queens" = "Rural",
+      "Richmond" = "Urban",
+      "Saratoga" = "Rural",
+      "Dutchess" = "Rural",
+      "Orange" = "Rural",
+      "Rockland" = "Rural",
+      "Sullivan" = "Rural",
+      "Oneida" = "Rural",
+      "Schenectady" = "Rural",
+      "Ulster" = "Rural",
+      "Bronx" = "Urban",
+      "Niagara" = "Rural",
+      "Chemung" = "Rural",
+      "Broome" = "Rural",
+      "Rensselaer" = "Rural",
+      "Putnam" = "Rural",
+      "Montgomery" = "Rural",
+      "Clinton" = "Rural",
+      "Columbia" = "Rural",
+      "Tompkins" = "Rural",
+      "Wayne" = "Rural",
+      "Chenango" = "Rural",
+      "Oswego" = "Rural",
+      "Orleans" = "Rural",
+      "Steuben" = "Rural",
+      "Otsego" = "Rural",
+      "Chautauqua" = "Rural",
+      "Ontario" = "Rural",
+      "Cayuga" = "Rural",
+      "Madison" = "Rural",
+      "Schoharie" = "Rural",
+      "Livingston" = "Rural",
+      "Saint Lawrence" = "Rural",
+      "Warren" = "Rural",
+      "Essex" = "Rural",
+      "Genesee" = "Rural",
+      "Herkimer" = "Rural",
+      "Greene" = "Rural",
+      "Seneca" = "Rural",
+      "Washington" = "Rural",
+      "Fulton" = "Rural",
+      "Tioga" = "Rural",
+      "Delaware" = "Rural",
+      "Yates" = "Rural",
+      "Allegany" = "Rural",
+      "Jefferson" = "Rural",
+      "Wyoming" = "Rural",
+      "Schuyler" = "Rural",
+      "Franklin" = "Rural",
+      "Cattaraugus" = "Rural",
+      "Lewis" = "Rural"
+    )
+  }
+  
+  value_mapping <- c("S" = 5, "R" = 4, "I" = 3, "TF" = 2, "N/I" = 1, "None" = 0)
+  
+  columns_to_transform <- c(15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45)
+  
+  for (col in columns_to_transform) {
+    dataframe[[col]] <- ifelse(dataframe[[col]] %in% names(value_mapping),
+                            value_mapping[dataframe[[col]]],
+                            dataframe[[col]])
+  }
+  
+  dataframe[columns_to_transform] <- lapply(dataframe[columns_to_transform], as.numeric)
+  
+  return(dataframe)
+}
+
+# Run Urban vs Rural Logistic Regression before application loads
+run_region_lr <- function() {
+  print("Running Logistic Regression...")
+  amr_df <- read_parquet("amr_data.parquet")
+  
+  # Add urban or rural region to region column
+  amr_df <- add_regions(amr_df)
+  amr_df$region <- as.factor(amr_df$region)
+  
+  # Split data into training and testing sets with an 80/20 proportion
+  set.seed(531)
+  split <- initial_split(amr_df, prop = 0.8, strata = NULL)
+  
+  train <- split %>% 
+    training()
+  test <- split %>% 
+    testing()
+  
+  # Train the model
+  model <- glm(region ~ species + R1 + H1 + W1 + N5 + N3 + I2 + E1 + O1 + N4 + N6 + N2 + B1 + Z1 + W2 + Z2 + F1 + M2 + T1 + K1 + S1 + D3 + B2 + L1 + W4 + H3 + H4 + X1, family = binomial, data = train)
+  
+  # Make predictions
+  predicted_probabilities <- predict(model, newdata = test, type="response")
+  Predictions <- ifelse(predicted_probabilities > 0.5, "Urban", "Rural")
+  
+  # Generate confusion matrix based on prediction results
+  conf_matrix <- table(Predictions, test$region)
+  confusionMatrix(conf_matrix)
+  
+  print("Logistic Regression Completed!")
+  result <- confusionMatrix(conf_matrix)
+  return(result)
+}
+
 function(input, output, session) {
   # CANINE VS FELINE CSV FILE LOADING
   cvf_svm_data <- read.csv("svm_urine.csv")
   
+  # URBAN VS RURAL PARQUET FILE LOADING
+  region_lr_data <- read_parquet("amr_data.parquet")
+  region_lr_data <- add_regions(region_lr_data)
+  region_lr_data <- region_lr_data %>% select(id, state, county, region, everything())
+  region_lr_data <- region_lr_data %>% select(-Z4, -U1, -Z3, -W5, -J1, -O3, -V1, -I1, -G1, -A1, -R3, -"FUSIDIC ACID", -B3, -D4, -H2, -CEFOPERAZONE, -N1, -C1, -R2, -P1, -D2, -W3, -Q1, -H5, -M1, -Y1)
+
   # Reactive value to track if preloading is complete
   preloading_done <- reactiveVal(FALSE)
   
@@ -83,11 +209,17 @@ function(input, output, session) {
   cvf_svm_confusion_matrix <- reactiveVal(NULL)
   cvf_svm_tsne_data<- reactiveVal(NULL)
   
-  # When the applications tarts, execute any preload functions
+  # Urban vs Rural Logistic Regression confusion matrix
+  region_lr_confusion_matrix <- reactiveVal(NULL)
+  
+  # When the applications starts, execute any preload functions
   observe({
     results <- run_cvf_svm()
     cvf_svm_confusion_matrix(results$confusion_matrix)
     cvf_svm_tsne_data(results$tsne_data)
+    
+    # Urban vs Rural logistic regression
+    region_lr_confusion_matrix(run_region_lr())
     
     # Set preloading_done to TRUE when finished all preloading
     preloading_done(TRUE)
@@ -273,7 +405,7 @@ function(input, output, session) {
           <p>In this case, the logistic regression model was used to attempt to classify whether antimicrobial resistant bacteria found in canines and felines differ between urban and rural regions based on susceptibility test results the model was trained on.</p>
           '),
           hr(),
-          radioButtons("region_svm_radio", NULL, c("Available Data" = "avail", "Confusion Matrix" = "conf")),
+          radioButtons("region_lr_radio", NULL, c("Available Data" = "avail", "Confusion Matrix" = "conf")),
         )
       )
     } else {
@@ -286,28 +418,95 @@ function(input, output, session) {
   })
   
   # Urban vs Rural logistic regression radio button
-  observeEvent(input$region_svm_radio, {
-    selected_option <- input$region_svm_radio
+  observeEvent(input$region_lr_radio, {
+    selected_option <- input$region_lr_radio
     
     print(paste("Selected option:", selected_option))
   })
   
   output$region_conditional_output <- renderUI({
     selected_method <- input$region_method
-    selected_option <- input$region_svm_radio
+    selected_option <- input$region_lr_radio
     
     if (selected_method == "lr") {
       if (selected_option == "avail") {
+        print(region_lr_confusion_matrix())
         return (
-          p("selected avail")
+          list(
+            HTML('
+              <h4><b>Available Data</b></h4>
+              <hr/>
+              <p>The original dataset consisted of 111,969 infections identified in canines and felines.</p>
+              <p>There was some preliminary data analysis that had to be conducted to the existing dataset to identify whether a specific county was in a rural or urban region.</p>
+              <p>To indentify whether a county was in a rural or urban region, we referenced the following:</p>
+              <p><i>New York State Rural Counties and Towns - New York State Department of ... New York State Department of Health. (2021, August). <a href="https://www.health.ny.gov/funding/soi/20283/docs/attachment7.pdf">https://www.health.ny.gov/funding/soi/20283/docs/attachment7.pdf</a></i></p>
+              <p>A new feature was added to the dataset - "region" - which identifies whether the county was in a rural or urban region.</p>
+              <p>Susceptibility test results for the various drugs were also converted to a numerical value before using the logistic regression model:</p>
+              <ul>
+                <li>S (Susceptible) = 5</li>
+                <li>R (Resistant) = 4</li>
+                <li>I (Intermediate) = 3</li>
+                <li>TF (To Follow) = 2</li>
+                <li>N/I (Not interpretable) = 1</li>
+                <li>None (Empty) = blank</li>
+              </ul>
+              <p>When dividing the data into training data and testing data, an 80%/20% split was used.</p>
+              <p>After including the dataset to add the "region" feature, this is a small example of how the available data for the logistic regression model looks like.</p>
+              <p>Note: The drugs only include R1 - T2, as any drug after those have no test results in any cases</p>
+            '),
+            dataTableOutput("region_lr_data_table")
+          )
         )
       } else if (selected_option == "conf") {
         return (
-          p("selected conf")
+          list(
+            HTML('
+              <h4><b>Confusion Matrix and Statistics</b></h4>
+              <hr/>
+            '),
+            verbatimTextOutput("region_lr_confusion_matrix_output"),
+            HTML('
+              <p>Shown above is the generated confusion matrix and statistics from the trained logistic regression model on this particular run.</p>
+              <p>As the training and testing data is randomly selected each run, the values will almost never be the same on consecutive runs.</p>
+              <p>Here is some insight on how the interpret these values:</p>
+              <p><b>Confusion Matrix: </b>The two rows represent the Urban/Rural predictions from the trained logistic regression model. The two columns represent the actual number of Urban/Rural regions in the testing data. For example, the top-left value shows the amount of times the model predicted rural and the actual testing data was rural. On the other hand, for the top-right value, it shows the amount of times the model predicted urban but the actual testing data was urban. If the row and column name are the same (e.g. Urban-Urban or Rural-Rural), the value in that cell indicates how many predictions the model got correct.</p>
+              <p><b>Accuracy: </b>This value represents the overall accuracy of the model in correctly predicting whether the county is in an Urban or Rural region based on the susceptibility tests.</p>
+              <p><b>Kappa: </b>Measures the agreement between classification and truth values. A value closer to 1 indicates good agreement, while a value closer to 0 indicates no agreement.</p>
+              <p><b>Sensitivity: </b>The proportion of actual positives (Rural) correctly predicted.</p>
+              <p><b>Specificity: </b>The proportion of actual negatives (Urban) correctly predicted.</p>
+              <p><b>Prevalence: </b>The proportion of actual positives (Rural) in the dataset. </p>
+              <p><b>Balanced Accuracy: </b>The determined average of sensitivity and specificity. May be useful for imbalanced datasets</p>
+              <br/>
+              <p><b>Confusion Matrix and Statistics Conclusion</b></p>
+              <p>After multiple runs of training and testing the logistic regression model, we have come to the same conclusion every time when analyzing the confusion matrix and statistics.</p>
+              <p>The overall accuracy is around chance level, and it is difficult to conclude whether the types of antibiotic-resistant bacteria found in canines and felines differ between urban and rural regions.</p>
+              <p>However, the high sensitivity of the logistic regression model meants that it is relatively good at correctly identifying instances from rural regions when given susceptibility tests from rural regions. But, the low specificity for the urban class suggests that the model struggles to correctly identify instances from urban regions.</p>
+            ')
+          )
         )
       } 
     } else {
       NULL
     }
   })
+  
+  # Render the Urban vs Rural logistic regression confusion matrix
+  output$region_lr_confusion_matrix_output<- renderPrint({
+    # Get the confusion matrix from reactive value
+    confusion_matrix <- region_lr_confusion_matrix()
+    if (!is.null(confusion_matrix)) {
+      print(confusion_matrix)
+    } else {
+      "Confusion matrix not available yet."
+    }
+  })
+  
+  # Render the logistic regression available data table within the conditional output
+  output$region_lr_data_table <- renderDataTable({
+    region_lr_data[1:50, ]
+  }, options = list(
+    scrollX = TRUE,
+    pageLength = 5
+    
+  ))
 }
